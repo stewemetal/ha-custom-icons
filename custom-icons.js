49f50ae898e2861b8ed27c3344c4aa7d8cd299e0
István Juhos <stewemetal@gmail.com>
2024-11-11 18:14:26 +0100

    Initial commit with Tesla icon

---
+++ custom-icons.js
@@ -0,0 +1,31 @@
+var icons = {
+  "example_multi":[0,0,100,100,"M 50 10 A 40 40 0 1 0 50 90 A 40 40 0 1 0 50 10 Z M 50 30 A 20 20 0 1 1 50 70 A 20 20 0 1 1 50 30 Z M0,100 50,25 50,75 100,0 "],
+  "tesla":[0,0,32,32,"M16 7.151l3.302-4.036c0 0 5.656 0.12 11.292 2.74-1.443 2.182-4.307 3.25-4.307 3.25-0.193-1.917-1.536-2.385-5.807-2.385l-4.479 25.281-4.51-25.286c-4.24 0-5.583 0.469-5.776 2.385 0 0-2.865-1.057-4.307-3.24 5.635-2.62 11.292-2.74 11.292-2.74l3.302 4.031h-0.005zM16 1.953c4.552-0.042 9.766 0.703 15.104 3.036 0.714-1.292 0.896-1.859 0.896-1.859-5.833-2.313-11.297-3.109-16-3.13-4.703 0.021-10.167 0.813-16 3.13 0 0 0.26 0.703 0.896 1.865 5.339-2.344 10.552-3.083 15.104-3.047z "],
+  }
+
+async function getIcon(name) {
+  if (!(name in icons)) {
+    console.log(`Icon "${name}" not available`);
+    return '';
+  }
+
+  var svgDef = icons[name];
+  var primaryPath = svgDef[4];
+  return {
+    path: primaryPath,
+    viewBox: svgDef[0] + " " + svgDef[1] + " " + svgDef[2] + " " + svgDef[3]
+  }
+
+}
+
+async function getIconList() {
+  return Object.entries(icons).map(([icon]) => ({
+    name: icon
+  }));
+}
+
+window.customIconsets = window.customIconsets || {};
+window.customIconsets["custom"] = getIcon;
+
+window.customIcons = window.customIcons || {};
+window.customIcons["custom"] = { getIcon, getIconList };
